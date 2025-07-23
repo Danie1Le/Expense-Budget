8dc46efdb7e649c28e5183b466d87ee67c41ce3a
Danie1Le <danielle8262005@gmail.com>
2025-07-22 20:42:02 -0700

    deploy app

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,8 +1,17 @@
-// Import the functions you need from the SDKs
+﻿// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { firebaseConfig } from './config.js';
+
+// Firebase configuration - Will be replaced during GitHub Actions deploy
+const firebaseConfig = {
+    apiKey: "REPLACE_API_KEY",
+    authDomain: "REPLACE_AUTH_DOMAIN",
+    projectId: "REPLACE_PROJECT_ID",
+    storageBucket: "REPLACE_STORAGE_BUCKET",
+    messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
+    appId: "REPLACE_APP_ID"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -11,4 +20,3 @@ const db = getFirestore(app);
 
 // Export the Firebase services
 export { app, auth, db };
-
